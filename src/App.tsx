f768b7c87d7ca680a286aff3ee059b5858e03902
mahabubsaki <md929820@gmail.com>
2023-03-20 15:08:20 +0600

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import Tone from 'tone';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const synth = new Tone.Synth().toDestination();
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <p>
-          <p>
-            <p>
-              <p onClick={() => synth.triggerAttackRelease("C4", "8N")}>sds</p>
-            </p>
-          </p>
-        </p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -30,14 +22,14 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
